8f5c7d5c8caa8bf4bb0bfe2d08e4031bbf07fa9a
ruanyouxing <ruanyouxing@gmail.com>
2022-05-27 03:27:14 +0700

    wilder

--- lua/editor.lua
+++ lua/editor.lua
@@ -499,22 +499,3 @@ wilder.set_option('pipeline', {
     left = {' ', wilder.wildmenu_spinner(), ' '},
     right = {' ', wilder.wildmenu_index(), ' '},
   })}))
--- vim.cmd[[
--- 	call wilder#set_option('pipeline',[
--- 		\ wilder#branch(wilder#cmdline_pipeline({'use_python': 0,'fuzzy': 1,
--- 		\ 'fuzzy_filter':
--- 			\ wilder#lua_fzy_filter()}),
--- 		\ wilder#vim_search_pipeline(),
--- 		\ [wilder#check({_, x -> empty(x)}),
--- 		\ wilder#history(),
--- 		\ wilder#result({'draw': [{_, x -> ' ' . x}]})])])
--- 	call wilder#set_option('renderer',
--- 		\ wilder#renderer_mux({':':
--- 		\ wilder#popupmenu_renderer({'highlighter':
--- 		\ wilder#lua_fzy_highlighter(),
--- 		\ 'left': [wilder#popupmenu_devicons()],
--- 		\ 'right': [' ', wilder#popupmenu_scrollbar()]}), '/':
--- 		\ wilder#wildmenu_renderer({'highlighter':
--- 		\ wilder#lua_fzy_highlighter()})}))
--- ]]
-
