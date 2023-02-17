94268bc628589ae808c2ead8e20729ccf362b2dd
ruanyouxing <ruanyouxing7@gmail.com>
2023-02-17 19:28:16 +0700

    merge keymap configs

--- lua/keymap/config.lua
+++ lua/keymap/config.lua
@@ -1,9 +1,11 @@
 local keymap = require 'core.keymap'
-local nmap, imap, xmap, vmap = keymap.nmap, keymap.imap, keymap.xmap, keymap.vmap
+local buf = vim.lsp.buf
+local nmap, imap, xmap, vmap, xnmap = keymap.nmap, keymap.imap, keymap.xmap, keymap.vmap, keymap.xnmap
 local silent = keymap.silent
 local opts = keymap.new_opts
 local cmd, cu = keymap.cmd, keymap.cu
 local plug = keymap.plug
+_G.OnFocus = 0
 vim.g.mapleader = ' '
 nmap { ' ', '' }
 xmap { ' ', '' }
@@ -62,30 +64,33 @@ nmap {
     '<leader>cp',
     function()
       local Terminal = require('toggleterm.terminal').Terminal
-      local terminalOpts = {
+      local termOpts = {
         cmd = nil,
         close_on_exit = false,
         dir = vim.fn.getcwd(),
         hidden = true,
-        direction = 'float',
+        direction = 'tab',
       }
-      if vim.bo.filetype == 'cpp' then
-        local outputFileName = nil
-        vim.ui.input({ prompt = 'File name: ' }, function(input)
-          outputFileName = input
-        end)
-        terminalOpts['cmd'] = 'g++ '
+      if vim.bo.filetype == 'cpp' or vim.bo.filetype == 'c' then
+        local compiler_name = ''
+        if vim.bo.filetype == 'cpp' then
+          compiler_name = 'g++ '
+        else
+          compiler_name = 'gcc '
+        end
+        local outputFileName = vim.fn.input { prompt = 'File name: ', cancelreturn = vim.fn.expand '%:r' .. '.o' }
+        termOpts['cmd'] = compiler_name
             .. vim.fn.expand '%:p'
             .. ' -o '
             .. outputFileName
             .. ' && '
             .. './'
             .. outputFileName
-        local compile = Terminal:new(terminalOpts)
+        local compile = Terminal:new(termOpts)
         compile:toggle()
       elseif vim.bo.filetype == 'python' then
-        terminalOpts['cmd'] = 'python ' .. vim.fn.expand '%:p'
-        local compile = Terminal:new(terminalOpts)
+        termOpts['cmd'] = 'python ' .. vim.fn.expand '%:p'
+        local compile = Terminal:new(termOpts)
         compile:toggle()
       end
     end,
@@ -125,6 +130,52 @@ nmap {
       end
     end,
   },
+  {
+    'K',
+    function()
+      local winid = require('ufo').peekFoldedLinesUnderCursor()
+      if not winid then
+        vim.lsp.buf.hover()
+      end
+    end,
+  },
+}
+
+-- Folds
+nmap {
+  {
+    'zR',
+    function()
+      require('ufo').openAllFolds()
+    end,
+  },
+  {
+    'zM',
+    function()
+      require('ufo').closeAllFolds()
+    end,
+  },
+}
+-- LSP
+nmap {
+  {
+    '<leader>ca',
+    function()
+      buf.code_action()
+    end,
+  },
+  {
+    '<leader>r',
+    function()
+      buf.references()
+    end,
+  },
+  {
+    'R',
+    function()
+      buf.rename()
+    end,
+  },
 }
 nmap {
   { 'j', plug 'faster_move_j',  opts(silent) },
@@ -138,7 +189,83 @@ imap {
   { '<C-i>', cmd 'IconPickerInsert alt_font symbols nerd_font emoji' },
 }
 
+-- Cokeline
+nmap {
+  { '<C-]>',   plug 'cokeline-focus-next' },
+  { '<C-[>',   plug 'cokeline-focus-prev' },
+  { '<Tab>',   plug 'cokeline-switch-next' },
+  { '<S-Tab>', plug 'cokeline-switch-prev' },
+}
+for i = 1, 9 do
+  nmap {
+    { ('<F%s>'):format(i),      (plug 'cokeline-focus-%s)'):format(i) },
+    { ('<Leader>%s'):format(i), (plug 'cokeline-switch-%s)'):format(i) },
+  }
+end
 for i = 1, 9 do
   vmap { ('<F%s>'):format(i), (cu(cmd 'HSHighlight %s<CR>'):format(i)) }
 end
 vmap { 'dh', 'HSRmHighlight' }
+vim.keymap.set('n', '<CR>', function()
+  vim.cmd 'NeoZoomToggle'
+end, { silent = true, nowait = true })
+
+-- Zen mode
+local function load_autocmds()
+  local autocmd = vim.api.nvim_create_autocmd
+  vim.api.nvim_create_augroup('galaxyline', { clear = true })
+  local options = {
+    pattern = '*',
+    group = 'galaxyline',
+    callback = function()
+      require('galaxyline').load_galaxyline()
+    end,
+  }
+  local events = {
+    'BufEnter',
+    'BufReadPost',
+    'BufWinEnter',
+    'BufWritePost',
+    'ColorScheme',
+    'FileChangedShellPost',
+    'FileType',
+    'TermOpen',
+    'VimResized',
+    'WinEnter',
+  }
+  autocmd(events, options)
+  autocmd({ 'WinLeave' }, {
+    pattern = '*',
+    group = 'galaxyline',
+    callback = function()
+      require('galaxyline').inactive_galaxyline()
+    end,
+  })
+end
+xnmap {
+  { 'p',  plug 'YankyPutAfter' },
+  { 'P',  plug 'YankyPutBefore' },
+  { 'gp', plug 'YankyGPutAfter' },
+  { 'gP', plug 'YankyGPutBefore' },
+  { 'y',  plug 'YankyYank' },
+  {
+    '<C-S-y>',
+    function()
+      require('telescope').extensions.yank_history.yank_history()
+    end,
+  },
+}
+vim.keymap.set('n', '<F11>', function()
+  if OnFocus == 0 then
+    OnFocus = 1
+    vim.api.nvim_del_augroup_by_name 'galaxyline'
+    vim.o.statusline = 0
+    vim.cmd [[TZAtaraxis]]
+  else
+    vim.cmd [[TZAtaraxis]]
+    require('galaxyline').load_galaxyline()
+    load_autocmds()
+    require('codewindow').open_minimap()
+    OnFocus = 0
+  end
+end, { noremap = true, silent = true })
