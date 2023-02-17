3ff18f173bc8ca2f72cc9c3336f5e6b3fb990e50
ruanyouxing <ruanyouxing7@gmail.com>
2023-02-17 19:28:46 +0700

    treesitter playgrond

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -146,6 +146,7 @@ function editor.treesitter()
       'html',
       'javascript',
       'typescript',
+      'query',
       'css',
     },
     sync_install = false,
@@ -222,6 +223,24 @@ function editor.treesitter()
           ['<leader>df'] = '@function.outer',
           ['<leader>dF'] = '@class.outer',
         },
+      },
+    },
+    playground = {
+      enable = true,
+      disable = {},
+      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = false, -- Whether the query persists across vim sessions
+      keybindings = {
+        toggle_query_editor = 'o',
+        toggle_hl_groups = 'i',
+        toggle_injected_languages = 't',
+        toggle_anonymous_nodes = 'a',
+        toggle_language_display = 'I',
+        focus_language = 'f',
+        unfocus_language = 'F',
+        update = 'R',
+        goto_node = '<cr>',
+        show_help = '?',
       },
     },
   }
