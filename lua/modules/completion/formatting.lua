efedab01c8622786dc1cd3bb9425def21fbcf7b7
ruanyouxing <ruanyouxing7@gmail.com>
2023-07-09 13:29:38 +0700

    move nixpkgs-fmt to alejandra

--- lua/modules/completion/formatting.lua
+++ lua/modules/completion/formatting.lua
@@ -12,7 +12,7 @@ function format.setup()
     },
     formatting.prettier,
     formatting.clang_format,
-    formatting.nixpkgs_fmt,
+    formatting.alejandra,
     diagnostics.eslint.with {
       extra_args = { '--config', vim.fn.expand(config_dir .. '/.eslintrc.json') },
     },
