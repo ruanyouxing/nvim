3d9caebe6a9dfde7af187e026a5a399d2902daa4
ruanyouxing <ruanyouxing7@gmail.com>
2023-11-13 14:16:20 +0700

    move clangd to config.lua

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -4,6 +4,55 @@ function lang.headlines()
   require('headlines').setup {}
 end
 
+function lang.clangd_setup()
+  require('clangd_extensions').setup {
+    extensions = {
+      autoSetHints = true,
+      hover_with_actions = true,
+      inlay_hints = {
+        only_current_line = false,
+        only_current_line_autocmd = 'CursorHold',
+        show_parameter_hints = true,
+        parameter_hints_prefix = '<- ',
+        other_hints_prefix = '=> ',
+        max_len_align = false,
+        max_len_align_padding = 1,
+        right_align = false,
+        right_align_padding = 7,
+        highlight = 'Comment',
+        priority = 100,
+      },
+      ast = {
+        role_icons = {
+          type = '',
+          declaration = '',
+          expression = '',
+          specifier = '',
+          statement = '',
+          ['template argument'] = '',
+        },
+        {
+          Compound = '',
+          Recovery = '',
+          TranslationUnit = '',
+          PackExpansion = '',
+          TemplateTypeParm = '',
+          TemplateTemplateParm = '',
+          TemplateParamObject = '',
+        },
+        highlights = {
+          detail = 'Comment',
+        },
+        memory_usage = {
+          border = 'rounded',
+        },
+        symbol_info = {
+          border = 'rounded',
+        },
+      },
+    },
+  }
+end
 function lang.filetypes()
   vim.g.did_load_filetypes = 1
   require('filetype').setup {
--- lua/modules/lang/lsp.lua
+++ lua/modules/lang/lsp.lua
@@ -60,56 +60,6 @@ function lsp.lspconfig()
   }
 end
 
-function lsp.clangd_setup()
-  require('clangd_extensions').setup {
-    extensions = {
-      autoSetHints = true,
-      hover_with_actions = true,
-      inlay_hints = {
-        only_current_line = false,
-        only_current_line_autocmd = 'CursorHold',
-        show_parameter_hints = true,
-        parameter_hints_prefix = '<- ',
-        other_hints_prefix = '=> ',
-        max_len_align = false,
-        max_len_align_padding = 1,
-        right_align = false,
-        right_align_padding = 7,
-        highlight = 'Comment',
-        priority = 100,
-      },
-      ast = {
-        role_icons = {
-          type = '',
-          declaration = '',
-          expression = '',
-          specifier = '',
-          statement = '',
-          ['template argument'] = '',
-        },
-        {
-          Compound = '',
-          Recovery = '',
-          TranslationUnit = '',
-          PackExpansion = '',
-          TemplateTypeParm = '',
-          TemplateTemplateParm = '',
-          TemplateParamObject = '',
-        },
-        highlights = {
-          detail = 'Comment',
-        },
-        memory_usage = {
-          border = 'rounded',
-        },
-        symbol_info = {
-          border = 'rounded',
-        },
-      },
-    },
-  }
-end
-
 function lsp.lsputils()
   local handlr = vim.lsp.handlers
   local sym = require 'lsputil.symbols'
