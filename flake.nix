bbe770fe5bbcac20a71a9aeef24aeb3fd5aa7ce6
ruanyouxing <ruanyouxing7@gmail.com>
2025-12-07 17:32:09 +0700

    test

--- flake.nix
+++ flake.nix
@@ -5,22 +5,48 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    root = ./.;
-  in {
-    packages.${system}.default = pkgs.stdenv.mkDerivation {
-      name = "nvim-config";
-      phases = ["installPhase"];
-      installPhase = ''
-        mkdir -p $out
-        cp -r ${root} $out
-      '';
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      root = ./.;
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "nvim-config";
+        phases = [ "installPhase" ];
+        installPhase = ''
+          mkdir -p $out
+          cp -r ${root} $out
+        '';
+      };
+      neovim = pkgs.neovim;
+      utils = with pkgs; [
+        gcc
+        gnumake
+        fd
+        fzf
+        ripgrep
+        lazygit
+        luajit
+        nodejs
+        yarn
+        python311Packages.pip
+        haskellPackages.greenclip
+        eslint_d
+        nodePackages.prettier
+        clang-tools
+        ccls
+        lldb
+        python310Packages.debugpy
+        black
+        stylua
+        shellcheck
+        alejandra
+      ];
     };
-    packages.neovim = pkgs.neovim-unwrapped;
-  };
 }
