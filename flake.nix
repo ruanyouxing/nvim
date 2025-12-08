4ace6df47af33f653443a12fad921a325079ff39
ruanyouxing <ruanyouxing7@gmail.com>
2025-12-08 14:39:17 +0700

    test 4

--- flake.nix
+++ flake.nix
@@ -5,62 +5,57 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      root = ./.;
-      utils = with pkgs; [
-        gcc
-        gnumake
-        fd
-        fzf
-        ripgrep
-        lazygit
-        luajit
-        nodejs
-        yarn
-        python311Packages.pip
-        haskellPackages.greenclip
-        eslint_d
-        nodePackages.prettier
-        clang-tools
-        ccls
-        lldb
-        python310Packages.debugpy
-        black
-        stylua
-        shellcheck
-        alejandra
-        lua-language-server
-      ];
-    in
-    {
-      hmConfig = {
-        home = {
-          sessionVariables = {
-            NVIM_APPNAME = "nvim-config";
-          };
-          packages = utils;
-          file.".config/nvim-config".source = root;
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    root = ./.;
+  in {
+    hmConfig = {
+      home = {
+        sessionVariables = {
+          NVIM_APPNAME = "nvim-config";
         };
-        programs.neovim = {
-          enable = true;
-          defaultEditor = true;
-          package = pkgs.neovim;
-        };
+        packages = with pkgs; [
+          gcc
+          gnumake
+          fd
+          fzf
+          ripgrep
+          lazygit
+          luajit
+          nodejs
+          yarn
+          python311Packages.pip
+          haskellPackages.greenclip
+          eslint_d
+          nodePackages.prettier
+          clang-tools
+          ccls
+          lldb
+          python310Packages.debugpy
+          black
+          stylua
+          shellcheck
+          alejandra
+          lua-language-server
+        ];
+        file.".config/nvim-config".source = root;
       };
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        name = "nvim-config";
-        phases = [ "installPhase" ];
-        installPhase = ''
-          mkdir -p $out
-          cp -r ${root} $out
-        '';
+      programs.neovim = {
+        enable = true;
+        defaultEditor = true;
       };
     };
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      name = "nvim-config";
+      phases = ["installPhase"];
+      installPhase = ''
+        mkdir -p $out
+        cp -r ${root} $out
+      '';
+    };
+  };
 }
