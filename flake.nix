fa57fdcadc9035b8bb51c116fb16ab0d10f49fd5
ruanyouxing <ruanyouxing7@gmail.com>
2026-05-09 17:04:42 +0700

    test new flake

--- flake.nix
+++ flake.nix
@@ -1,52 +1,94 @@
 {
-  description = "ruanyouxing neovim config";
+  description = "neovim wrapper flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      flake-utils,
+      ...
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      root = ./.;
-    in
-    {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        name = "nvim-config";
-        phases = [ "installPhase" ];
-        installPhase = ''
-          mkdir -p $out
-          cp -r ${root} $out
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        extraPackages = with pkgs; [
+          lua-language-server
+          stylua
+          clang-tools
+          pyright
+          black
+          pylint
+          nil
+          nixpkgs-fmt
+          ripgrep
+          fd
+          tree-sitter
+          gcc
+          gnumake
+          fd
+          fzf
+          ripgrep
+          lazygit
+          luajit
+          nodejs
+          yarn
+          python311Packages.pip
+          haskellPackages.greenclip
+          eslint_d
+          nodePackages.prettier
+          clang-tools
+          ccls
+          lldb
+          python310Packages.debugpy
+          black
+          stylua
+          shellcheck
+          alejandra
+          lua-language-server
+        ];
+
+        wrappedNeovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
+          extraMakeWrapperArgs = ''--prefix PATH : "${pkgs.lib.makeBinPath extraPackages}"'';
+        };
+
+        runNvimLocal = pkgs.writeShellScriptBin "run-nvim" ''
+          if [ -f "$PWD/init.lua" ]; then
+            echo "Loading local configuration from $PWD/init.lua..."
+            exec ${wrappedNeovim}/bin/nvim -u "$PWD/init.lua" "$@"
+          else
+            echo "Warning: No init.lua found in the project root. Loading default configuration."
+            exec ${wrappedNeovim}/bin/nvim "$@"
+          fi
         '';
+      in
+      {
+        packages = {
+          default = wrappedNeovim;
+          neovim = wrappedNeovim;
+        };
+
+        apps = {
+          default = {
+            type = "app";
+            program = "${runNvimLocal}/bin/run-nvim";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ wrappedNeovim ] ++ extraPackages;
+        };
+      }
+    )
+    // {
+      overlays.default = final: prev: {
+        custom-neovim = self.packages.${prev.system}.neovim;
       };
-      utils = with pkgs; [
-        gcc
-        gnumake
-        fd
-        fzf
-        ripgrep
-        lazygit
-        luajit
-        nodejs
-        yarn
-        python311Packages.pip
-        haskellPackages.greenclip
-        eslint_d
-        nodePackages.prettier
-        clang-tools
-        ccls
-        lldb
-        python310Packages.debugpy
-        black
-        stylua
-        shellcheck
-        alejandra
-        lua-language-server
-      ];
     };
 }
