10ac48b83da1f0f17562bda35b67875ff08a3004
ruanyouxing <ruanyouxing7@gmail.com>
2026-05-11 01:46:50 +0700

    installing some first vim plugins

--- nix/plugins.nix
+++ nix/plugins.nix
@@ -1,33 +1,51 @@
-{pkgs, ...}: let
+{ pkgs, ... }:
+let
   nvim-treesitter-with-grammars = pkgs.vimPlugins.nvim-treesitter.withPlugins (
-    _:
-      with pkgs.vimPlugins.nvim-treesitter.grammarPlugins; [
-        bash
-        c
-        cpp
-        fish
-        lua
-        json
-        yaml
-        make
-        python
-        rust
-        nix
-        html
-        javascript
-        typescript
-        query
-        css
-        qmljs
-      ]
+    _: with pkgs.vimPlugins.nvim-treesitter.grammarPlugins; [
+      bash
+      c
+      cpp
+      fish
+      lua
+      json
+      yaml
+      make
+      python
+      rust
+      nix
+      html
+      javascript
+      typescript
+      query
+      css
+      qmljs
+    ]
   );
-  tool-plugins = with pkgs.vimPlugins; [
+  cmp-plugins = with pkgs.vimPlugins; [
+    nvim-cmp
+    cmp-nvim-lua
+    cmp-nvim-lsp
+    cmp-calc
+    cmp-latex-symbols
+    cmp-treesitter
+    cmp_luasnip
+    cmp-path
+    cmp-buffer
+    cmp-emoji
+    cmp-nvim-lsp-document-symbol
+    cmp-look
+    cmp-spell
+    luasnip
+    LuaSnip-snippets-nvim
+    # cmp-emoji
+    # cmp-render-markdown
   ];
-  ui-plugins = with pkgs.vimPlugins; [
+  telescope-plugins = with pkgs.vimPlugins; [
+    telescope-nvim
+    telescope-zoxide
   ];
-  editor-plugins = with pkgs.vimPlugins; [
-  ];
-in {
+in
+{
   plugins = {
     start = with pkgs.vimPlugins; [
       lazy-nvim
@@ -39,24 +57,30 @@ in {
       nvim-ts-context-commentstring
       catppuccin-nvim
     ];
-    opt = with pkgs.vimPlugins; [
-      nvim-navbuddy
-      which-key-nvim
-      dashboard-nvim
-      indent-blankline-nvim
-      noice-nvim
-      neo-tree-nvim
-      nvim-notify
-      statuscol-nvim
-      neovim-ayu
-      tokyonight-nvim
-      twilight-nvim
-      virt-column-nvim
-      wilder-nvim
-      bufdelete-nvim
-      undotree
-      nvim-comment
-    ];
+    opt =
+      with pkgs.vimPlugins;
+      [
+        nvim-navbuddy
+        which-key-nvim
+        dashboard-nvim
+        indent-blankline-nvim
+        noice-nvim
+        neo-tree-nvim
+        nvim-notify
+        statuscol-nvim
+        neovim-ayu
+        tokyonight-nvim
+        twilight-nvim
+        virt-column-nvim
+        wilder-nvim
+        bufdelete-nvim
+        undotree
+        nvim-comment
+        better-escape-nvim
+        none-ls-nvim
+      ]
+      ++ telescope-plugins
+      ++ cmp-plugins;
     dev.nvim = {
       pure = pkgs.lib.cleanSource ../.;
       impure = "/home/hungz/.dotfiles/nvim";
