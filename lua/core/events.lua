b8ed6b1c02dd0df89ad1d24c3ed4e5bdba9cd4b4
ruanyouxing <ruanyouxing7@gmail.com>
2026-05-11 00:16:15 +0700

    add treesitter autocommand

--- lua/core/events.lua
+++ lua/core/events.lua
@@ -54,3 +54,11 @@ autocmd({ 'FileType' }, {
 --     require('specs').show_specs()
 --   end,
 -- })
+local treesitter_augroup = vim.api.nvim_create_augroup("nvim_treesitter", { clear = true })
+autocmd('FileType', {
+  pattern = '*',
+  group = treesitter_augroup,
+  callback = function(args)
+    pcall(vim.treesitter.start, args.buf)
+  end,
+})
