0fdcdc36d46c4f3aa24e933cb43348ca9c397b37
ruanyouxing <ruanyouxing7@gmail.com>
2026-05-26 01:10:03 +0700

    h

--- nix/default.nix
+++ nix/default.nix
@@ -8,5 +8,9 @@
   extraBuilderArgs.meta = {
     description = "Neovim wrapped with mnw";
     longDescription = "Neovim with custom config, plugins, and LSP toolchain managed via mnw";
+    homepage = "https://neovim.io";
+    license = pkgs.lib.licenses.asl20;
+    platforms = pkgs.lib.platforms.all;
+    teams = [ ];
   };
 }
